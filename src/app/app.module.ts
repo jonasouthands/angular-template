89cae4836104fab961cd4d93d25a7cbe7f386600
jonasouthands <jonas@outhands.nl>
2021-10-06 16:03:00 +0200

    Added angular universal / server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
     PageComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule
   ],
